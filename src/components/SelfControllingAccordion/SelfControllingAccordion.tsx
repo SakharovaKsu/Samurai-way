705c301ff475eb104ea10cf666a9b9624e2af3f9
SakharovaKsu <nagis38@gmail.com>
2023-05-17 17:11:54 +0300

    Добавление колбека в не котролируемые компоненты
    для акордиона и звездочек star

--- src/components/SelfControllingAccordion/SelfControllingAccordion.tsx
+++ src/components/SelfControllingAccordion/SelfControllingAccordion.tsx
@@ -1,7 +1,12 @@
 import React, {useState} from 'react';
 
 type AccordionPropsType = {
-  titleValue: string,
+    titleValue: string
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    onClick: () => void
 }
 
 function SelfControllingAccordion(props: AccordionPropsType) {
@@ -11,8 +16,8 @@ function SelfControllingAccordion(props: AccordionPropsType) {
 
     return (
         <div>
-            <AccordionTitle title={ props.titleValue }/>
-            <button onClick={() => { setCollapsed(!collapsed)} }>+</button>
+            <AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}}/>
+            {/*<button onClick={() => {setCollapsed(!collapsed)}}>+</button>*/}
             {!collapsed && <AccordionBody/>}
             {/*  Если в пропсе пришло true, то отрисуется аккордион, так как сравнение идет дальше после оператора И  */}
             {/*  Если же приходит false, то не рисуется, так как после оператора И дальше не идет, останавливается работа  */}
@@ -20,25 +25,21 @@ function SelfControllingAccordion(props: AccordionPropsType) {
     )
 }
 
-type AccordionTitlePropsType = {
-  title: string
-}
-
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log('AccordionTitle rendering');
 
-    return <h3>{ props.title }</h3>
+    return <h3 onClick={props.onClick}>{props.title}</h3>
 }
 
 function AccordionBody() {
     console.log('AccordionBody rendering');
 
     return (
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }
 
--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,34 +1,39 @@
 import React, {useState} from 'react';
 
-function UncontrolledRating() {
-  console.log('Rating rendering');
-
-  const [value, setValue] = useState(0)
-
-  return (
-    <div>
-      <Star selected={ value > 0 }/><button onClick={() => {setValue(1)}}>1</button>
-      <Star selected={ value > 1 }/><button onClick={() => {setValue(2)}}>2</button>
-      <Star selected={ value > 2 }/><button onClick={() => {setValue(3)}}>3</button>
-      <Star selected={ value > 3 }/><button onClick={() => {setValue(4)}}>4</button>
-      <Star selected={ value > 4 }/><button onClick={() => {setValue(5)}}>5</button>
-    </div>
-  )
-}
-
 type StarPropsType = {
-  selected: boolean
+    selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+}
+function UncontrolledRating() {
+    console.log('Rating rendering');
+
+    const [value, setValue] = useState(0)
+
+    return (
+        <div>
+            {/* value закидываем данные, за что отвечает каждый star */}
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
+        </div>
+    )
 }
 
 function Star(props: StarPropsType) {
-  console.log('Star rendering');
+    console.log('Star rendering');
 
-  // return props.selected === true : <span><b>star </b></span> ? <span>star </span>
+    return <span
+        onClick={() => props.setValue(props.value)}>
+        {props.selected ? <b>star </b> : 'star '}
+    </span>
 
-  if(props.selected === true) {
-    return <span><b>star </b></span>
-  }
-  return <span>star </span>
+    // if(props.selected === true) {
+    //   return <span><b>star </b></span>
+    // }
+    // return <span>star </span>
 }
 
 export default UncontrolledRating;
